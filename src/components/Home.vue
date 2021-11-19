e7c05d0e6f9eeac813fa3a095a0d6c6b2cfabdc9
Hyebin <thsuekfk2@naver.com>
2021-11-20 02:58:49 +0900

    timetable 컴포넌트화

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,37 @@
 <template>
-    <div>
-        <main-template>
-            <template slot="header">
-            </template>
-            <main-container>
-                <span style="font-size: 20px">hello home!</span>
-            </main-container>
-            <template slot="tab-bar">
-            </template>
-        </main-template>
-    </div>
+  <div>
+    <main-template>
+      <template slot="header"> </template>
+      <main-container>
+        <span style="font-size: 20px">hello home!</span>
+      </main-container>
+      <template slot="tab-bar"> </template>
+    </main-template>
+  </div>
 </template>
 
 <script>
-import MainTemplate from './MainTemplate.vue';
-import MainContainer from './MainContainer.vue';
+import MainTemplate from "./MainTemplate.vue";
+import MainContainer from "./MainContainer.vue";
 
-export default{
+export default {
   components: { MainTemplate, MainContainer },
 
-    name : 'home',
-    data() {
-        return {  
-
-
-        }
-    }
-}
+  name: "home",
+  data() {
+    return {};
+  },
+};
 //450px
 </script>
 <style scoped>
-    @media only screen and (max-width: 1024px) {  /* 테블릿 M일 때*/
-        
-    }
-    @media only screen and (max-width: 768px) { /* 테블릿S일 때 */
-
-    }
-    @media only screen and (max-width: 479px) { /* 모바일 일 때 */
-
-    }
+@media only screen and (max-width: 1024px) {
+  /* 테블릿 M일 때*/
+}
+@media only screen and (max-width: 768px) {
+  /* 테블릿S일 때 */
+}
+@media only screen and (max-width: 479px) {
+  /* 모바일 일 때 */
+}
 </style>
