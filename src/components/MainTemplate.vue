67ed6d82386a3c8aea50d74a64c722c57d42634d
Hyebin <thsuekfk2@naver.com>
2021-11-23 00:07:36 +0900

    더보기 스크롤 문제 리팩토링

--- src/components/MainTemplate.vue
+++ src/components/MainTemplate.vue
@@ -64,6 +64,8 @@ export default {
   methods: {
     seeMoreBtn() {
       this.show = !this.show;
+      if (this.show) document.documentElement.style.overflow = "hidden";
+      else document.documentElement.style.overflow = "auto";
     },
   },
   components: {
@@ -96,7 +98,7 @@ export default {
   background-color: white;
   box-shadow: 0px -4px 3px rgba(50, 50, 50, 0.3);
   z-index: 2;
-  color:#576BCA;
+  color: #576bca;
 }
 .tab-bar a {
   display: flex;
@@ -104,20 +106,17 @@ export default {
   align-items: center;
   text-decoration: none;
   opacity: 0.4;
-  
 }
 .tab-bar span {
   font-size: 3.5rem;
-  
 }
 
 .tab-bar .tab-bar-inner-txt {
   font-size: 1.2rem;
-  
 }
-.tab-bar a.router-link-exact-active{
-  opacity:1;
-  color:#354076;
+.tab-bar a.router-link-exact-active {
+  opacity: 1;
+  color: #354076;
 }
 
 ·tab-bar-see-more {
@@ -171,7 +170,7 @@ export default {
   .bg {
     background-color: rgba(0, 0, 0, 0.5);
     width: 100%;
-    height: 550px;
+    height: 100%;
     position: absolute;
     top: 60px;
   }
